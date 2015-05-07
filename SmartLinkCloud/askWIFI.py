8af11e0b40b30b1d3fca4e46d1df8657da463276
quanpower <quanpower@gmail.com>
2015-05-08 00:30:05 +0800

    edit updateApps.py--

---
+++ SmartLinkCloud/askWIFI.py
@@ -0,0 +1,106 @@
+#!/usr/bin/python
+
+import sqlite3
+import urllib
+import urllib2
+import json
+
+##----------------------------------------------------------------------##
+
+def get_local_mac_address():
+    '''get local mac address'''
+    import uuid
+    node = uuid.getnode()
+    mac = uuid.UUID(int = node).hex[-12:]
+    return mac
+
+##----------------------------------------------------------------------##
+
+def get_local_MAC_ADDRESS():
+    '''get local MAC ADDRESS,upper mac address with the colon'''
+    MAC = get_local_mac_address().upper()
+    MAC_ADRESS = ":".join([MAC[e:e+2] for e in range(0,11,2)])
+    return MAC_ADRESS
+
+##----------------------------------------------------------------------##
+
+def get_ip_address(ifname):
+    '''get ip_address,use it need ifname,for example :'eth1'.'''
+    import socket
+    import fcntl
+    import struct
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip = socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+    return ip
+
+##----------------------------------------------------------------------##
+
+def get_terminal_id(dbname,mac):
+    '''get terminal id from pa_terminal'''
+
+    get_terminal_id_prefix_uri = "http://api1.juwan.cn/PhoneAssistantServer/template/getTerminalInfo.php?mac="
+    URI = get_terminal_id_prefix_uri + mac
+    print URI
+
+####################################################
+##         connect the sqlite3 database           ##
+####################################################
+
+    conn = sqlite3.connect(dbname)
+    curs = conn.cursor()
+
+####################################################
+##  retuen the row count of the pa_terminal table ##
+####################################################
+
+    result = curs.execute('select count(*) from pa_terminal')
+    count = result.fetchone()[0]
+    print "rowcount = ",count
+
+    if count > 0:
+        print "Already has terminalid"
+    else:
+        req = urllib2.Request(URI)
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+
+        print the_page
+
+        json_data = json.loads(the_page)
+        terminalid = json_data['terminalid']
+        print "terminalid in the jsondata is : " + terminalid
+
+####################################################
+##  insert terminalid into  pa_terminal table ##
+####################################################
+
+        curs.execute("INSERT INTO pa_terminal VALUES (?)",(terminalid,));
+
+        conn.commit()
+
+    curs.execute("SELECT terminalid FROM pa_terminal")
+    for row in curs.fetchall():
+        print "Now, terminal id is: " + row[0]
+
+    curs.close()
+    conn.close()
+
+##----------------------------------------------------------------------##
+
+if __name__ == '__main__':
+
+    dbname = 'SmartLinkCloud.db'
+
+    print "local mac address is : " + get_local_mac_address()
+    print "local MAC ADDRESS is : " + get_local_MAC_ADDRESS()
+    print "lo ip is : " + get_ip_address('lo')
+    print "eth1 ip is : " + get_ip_address('eth1')
+
+    mac = get_local_mac_address()
+    get_terminal_id(dbname,mac)
+
--- SmartLinkCloud/updateApps.py
+++ SmartLinkCloud/updateApps.py
@@ -23,10 +23,10 @@ def get_pa_apps_fields(dbname):
 
     curs.execute("SELECT sql FROM sqlite_master WHERE tbl_name = 'pa_apps' AND type = 'table'")
     for sql in curs.fetchone():
-        print sql
-        #fields_list = re.split(r',',sql)
-        fields_list = sql.split(',')
-        print fields_list
+        #print sql
+        fields_list = re.split(r',',sql)
+        #fields_list = sql.split(',')
+        #print fields_list
 
         pa_apps_fields_list = []
 
@@ -35,10 +35,10 @@ def get_pa_apps_fields(dbname):
             #match = re.match(r'\w+',pa_apps_fields.lstrip())
             match = pattern.match(pa_apps_fields.lstrip())
             if match:
-                print match.group()
+                #print match.group()
                 pa_apps_fields_list.append(match.group())
         pa_apps_fields_list[0] = u'appid'
-        print "-"*30
+        print "---------------------pa_apps_fields_list--------------------------------"
         print pa_apps_fields_list
     curs.close()
     conn.close()
@@ -70,26 +70,27 @@ def get_all_apps(dbname):
 
     get_apps_prefix_uri = "http://houtai.juwan.cn/PhoneAssistantServer/template/getAllApps_jwbox.php?terminalid="
     URI = get_apps_prefix_uri + terminalid
+    print "-------------------------------------URI----------------------------------------------"
     print URI
 
     req = urllib2.Request(URI)
     response = urllib2.urlopen(req)
     the_page = response.read()
     #the_page = response.read().decode('unicode-escape')
-
+    print "---------------------------------the_page----------------------------------------------"
     print the_page
-
-    from collections import OrderedDict
-    order_dict_array = json.loads(the_page, object_pairs_hook=OrderedDict);
-    print order_dict_array
+    json_dicts = json.loads(the_page, 'utf-8');
+    print "---------------------------------json_dicts-----------------------------------------"
+    print json_dicts
 
     curs.close()
     conn.close()
 
-    return order_dict_array
+    #return order_dict_array
+    return json_dicts
 ##-----------------------------------------------------------------------##
 
-def update_all_apps(dbname,pa_apps_fields_list,order_dicts_array):
+def update_all_apps(dbname,pa_apps_fields_list,json_dicts):
     '''update all apps'''
 
 ####################################################
@@ -99,33 +100,44 @@ def update_all_apps(dbname,pa_apps_fields_list,order_dicts_array):
     conn = sqlite3.connect(dbname)
     curs = conn.cursor()
 
-    for order_dicts in  order_dict_array:
-        for key,value in order_dicts.items():
-            if type(value) is types.UnicodeType:
-                print 'key=',key,'，value=',value.encode('utf-8')
-            else:
-                 print 'key=',key,'，value=',value
+    insert_lists= []
 
+    for apps_info_dict in json_dicts:
+        insert_list= []
+
+        for key,value in apps_info_dict.items():
+            if key in pa_apps_fields_list:
+                if type(value) is types.UnicodeType:
+                    insert_list.append(value.encode('utf-8'))
+                else:
+                    insert_list.append(value)
+        insert_lists.append(insert_list)
+
+    for app_list in insert_lists:
+        print "############################insert_list##################################"
+        for app_info in app_list:
+            print "------------------------app_info-----------------------------"
+            print app_info
 ####################################################
 ##  insert apps into  pa_apps table               ##
 ####################################################
 
-    #    curs.execute("INSERT INTO pa_apps VALUES (?)",(terminalid,));
+    curs.execute("INSERT INTO pa_apps VALUES (?)",(terminalid,));
 
-     #   conn.commit()
+    conn.commit()
 
 
     curs.close()
     conn.close()
 
-#def set_default_encoding():
-#    import sys
+# def set_default_encoding():
+#     import sys
 #
-#    print sys.getdefaultencoding()
+#     print sys.getdefaultencoding()
 #
-#    reload(sys)
-#    sys.setdefaultencoding('utf-8')
-#    print sys.getdefaultencoding()
+#     reload(sys)
+#     sys.setdefaultencoding('utf-8')
+#     print sys.getdefaultencoding()
 
 ##-----------------------------------------------------------------------##
 
@@ -134,6 +146,6 @@ if __name__ == '__main__':
     dbname = 'SmartLinkCloud.db'
     #set_default_encoding()
     pa_apps_fields_list = get_pa_apps_fields(dbname)
-    order_dict_array = get_all_apps(dbname)
-    update_all_apps(dbname,pa_apps_fields_list,order_dict_array)
+    json_dicts = get_all_apps(dbname)
+    update_all_apps(dbname,pa_apps_fields_list,json_dicts)
 
